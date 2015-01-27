14b2f8614ce30fe4212dc135f8a3c02be1653063
msuzoagu <msuzoagu@gmail.com>
2015-01-27 14:35:15 -0500

    corrects expectation for spec/001_hashes_spec.rb

--- spec/001_hashes_spec.rb
+++ spec/001_hashes_spec.rb
@@ -3,20 +3,21 @@ describe "Hashes" do
 
   it 'adds a new key for type and give it the value "dynamic"' do
     # write your code here!
-    expect(language[:type]).to eq("dynamic") 
+    expect(language[:type]).to eq("dynamic")
   end
 
   it 'prints out all the key/value pairs in the hash' do
     expect(STDOUT).to receive(:puts).with("name: Ruby")
     expect(STDOUT).to receive(:puts).with("creator: Yukihiro Matsumoto")
+    expect(STDOUT).to receive(:puts).with("type: dynamic")
     # write your code here!
   end
 
   it 'returns the value given key name' do
-    expect('__').to eq("Ruby") 
+    expect('__').to eq("Ruby")
   end
 
-  it 'returns the key creator given the value' do 
+  it 'returns the key creator given the value' do
     expect('__').to eq(:creator)
   end
 end
